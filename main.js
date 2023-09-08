2057d935ef3bd1a393b12e87fcfc8e1034010ab8
Obito2011 <141378054+Obito2011@users.noreply.github.com>
2023-09-07 20:38:41 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function predload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
